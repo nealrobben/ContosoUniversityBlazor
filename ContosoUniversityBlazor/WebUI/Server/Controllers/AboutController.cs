6969b9cd315156f43a128f4f5f3b5c99d5c300eb
Neal Robben <neal.robben@gmail.com>
2021-05-27 23:08:33 +0200

    Added controllers

---
+++ ContosoUniversityBlazor/WebUI/Server/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using ContosoUniversityBlazor.Application.Home.Queries.GetAboutInfo;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ContosoUniversityBlazor.WebUI.Controllers
+{
+    public class AboutController : ApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<AboutInfoVM>> GetAboutInfo()
+        {
+            var result = await Mediator.Send(new GetAboutInfoQuery());
+
+            return Ok(result);
+        }
+    }
+}
