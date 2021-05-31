8ccd21ad0e19317571c2f201e7f8efcf886320b6
Neal Robben <neal.robben@gmail.com>
2021-05-31 23:14:09 +0200

    Working on Configure in Startup

--- ContosoUniversityBlazor/WebUI/Server/Startup.cs
+++ ContosoUniversityBlazor/WebUI/Server/Startup.cs
@@ -63,14 +63,25 @@ namespace WebUI.Server
             {
                 app.UseDeveloperExceptionPage();
                 app.UseWebAssemblyDebugging();
+                app.UseDatabaseErrorPage();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
+
+            app.UseHealthChecks("/health");
 
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
+
+            app.UseSwaggerUi3(settings =>
+            {
+                settings.Path = "/api";
+                settings.DocumentPath = "/api/specification.json";
+            });
 
             app.UseRouting();
 
