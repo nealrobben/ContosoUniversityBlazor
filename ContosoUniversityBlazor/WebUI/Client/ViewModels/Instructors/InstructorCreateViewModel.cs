bd66344a6f417a05f248d2b1af48091ef24753b2
Neal Robben <neal.robben@gmail.com>
2021-07-08 21:54:26 +0200

    Implemented Instructor Create ViewModel

--- ContosoUniversityBlazor/WebUI/Client/ViewModels/Instructors/InstructorCreateViewModel.cs
+++ ContosoUniversityBlazor/WebUI/Client/ViewModels/Instructors/InstructorCreateViewModel.cs
@@ -1,13 +1,38 @@
-﻿using WebUI.Client.Services;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Threading.Tasks;
+using WebUI.Client.Services;
+using WebUI.Shared.Instructors.Commands.CreateInstructor;
 
 namespace WebUI.Client.ViewModels.Instructors
 {
     public class InstructorCreateViewModel : InstructorViewModelBase
     {
-        public InstructorCreateViewModel(InstructorService instructorService)
+        private readonly NavigationManager _navManager;
+
+        public CreateInstructorCommand CreateInstructorCommand = new CreateInstructorCommand() { HireDate = DateTime.UtcNow.Date };
+
+        public InstructorCreateViewModel(InstructorService instructorService,
+             NavigationManager navManager)
             :base(instructorService)
         {
+            _navManager = navManager;
+        }
 
+        public async Task FormSubmitted(EditContext editContext)
+        {
+            bool formIsValid = editContext.Validate();
+
+            if (formIsValid)
+            {
+                var result = await _instructorService.CreateAsync(CreateInstructorCommand);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    _navManager.NavigateTo("/instructors");
+                }
+            }
         }
     }
 }
