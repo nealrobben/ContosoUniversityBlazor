2e17ba8674348d2cf98dc8f6c3b2116ab7dcd24b
Neal Robben <neal.robben@gmail.com>
2021-08-10 23:08:44 +0200

    Moved code to code-behind class

---
+++ ContosoUniversityBlazor/WebUI/Client/Pages/Departments/Departments.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using WebUI.Client.ViewModels.Departments;
+
+namespace WebUI.Client.Pages.Departments
+{
+    public partial class Departments
+    {
+        public DepartmentsViewModel DepartmentsViewModel { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await DepartmentsViewModel.OnInitializedAsync();
+        }
+    }
+}
