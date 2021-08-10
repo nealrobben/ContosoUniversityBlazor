4670a7ed1b141a4752114eb75d2a8c9a774fc36d
Neal Robben <neal.robben@gmail.com>
2021-08-10 23:20:26 +0200

    Moved code to code-behind

---
+++ ContosoUniversityBlazor/WebUI/Client/Pages/Courses/CourseCreate.razor.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using WebUI.Client.ViewModels.Courses;
+
+namespace WebUI.Client.Pages.Courses
+{
+    public partial class CourseCreate
+    {
+        [Inject]
+        public CourseCreateViewModel CourseCreateViewModel { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await CourseCreateViewModel.OnInitializedAsync();
+        }
+    }
+}
--- ContosoUniversityBlazor/WebUI/Client/Pages/Departments/Departments.razor.cs
+++ ContosoUniversityBlazor/WebUI/Client/Pages/Departments/Departments.razor.cs
@@ -1,10 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
 using WebUI.Client.ViewModels.Departments;
 
 namespace WebUI.Client.Pages.Departments
 {
     public partial class Departments
     {
+        [Inject]
         public DepartmentsViewModel DepartmentsViewModel { get; set; }
 
         protected override async Task OnInitializedAsync()
